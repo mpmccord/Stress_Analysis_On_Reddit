38c172788e6373cf85ccc36497bfda39c42529f8
Melanie McCord <melanie.mccord19@ncf.edu>
2021-10-01 14:35:34 -0400

    Added bag of words with unknowns

--- text_cleaning.R
+++ text_cleaning.R
@@ -8,10 +8,18 @@ CleanText <- function(df) {
   # Creating list of words occuring in document
   # Select the text and the id to keep track of where the id occurred
   # Remove stopwords
+  # Remove punctuation
+  # Replace digits with d%d
   temp <- df %>%
     select(text, id) %>%
     unnest_tokens(word, text) %>%
-    anti_join(stop_words, by = c("word" = "word"))
+    anti_join(stop_words, by = c("word" = "word")) %>%
+    filter(
+      !str_detect(word, pattern = "[[:digit:]]"), # removes any words with numeric digits
+      !str_detect(word, pattern = "[[:punct:]]"), # removes any remaining punctuations
+      !str_detect(word, pattern = "(.)\\1{2,}"),  # removes any words with 3 or more repeated letters
+      !str_detect(word, pattern = "\\b(.)\\b")    # removes any remaining single letter words
+    )
   return(temp)
 }
 # CreateBagOfWords
@@ -34,25 +42,18 @@ CreateBagOfWords <- function(df) {
 # Parameters: 
 #             df: a dataframe, 
 #             smallest_count: the smallest number of occurrences you want to exclude (default: 1).
-CreateBagOfWordsWithUnknowns <- function(df, smallest_count=1) {
+CreateBagOfWordsWithUnknowns <- function(df, smallest_count=2) {
   # Smallest count allowed: removing the words that only appear a certain number of times.
   # If we look at the base dataset, the median number of times a word appears is once.
   # Since this is not meaningful, we will only include the words that appear more than once.
   
   # First, let's get the words and group them by id
-  words_and_id <- df %>%
-    select(text, id) %>%
-    unnest_tokens(word, text) %>%
-    anti_join(stop_words, by = c("word" = "word")) #%>%
+  words_and_id <- CleanText(df) %>%
+    group_by(id) %>%
+    count(word) %>%
+    mutate(word = if_else(n <= smallest_count, "unknown", word))
     #count(word)
   # mutate_all(~replace(., is.na(.), 0))
-  
-  # Now let's filter out the words that only appear once and mark them as "unknown"
-  text_counts <- words_and_id %>%
-    count(word)
-  bottom_percentile_of_words <- filter(text_counts, n <= smallest_count)$word
-  words_and_id <- words_and_id %>%
-    mutate(word = str_replace_all(word, pattern = bottom_percentile_of_words, replacement = rep("unknown", length(bottom_percentile_of_words))))
   
   # Now creating the dataframe of each id and each bag of words model.
   # We need to group by id (to preserve words by posts, 
@@ -62,9 +63,8 @@ CreateBagOfWordsWithUnknowns <- function(df, smallest_count=1) {
   token_text <- words_and_id %>%
     group_by(id) %>%
     count(word) %>%
-    tibble() %>%
-    pivot_wider(names_from = word, values_from = n, names_repair = "unique") %>%
-    mutate_all(~replace(., is.na(.), 0))
+    pivot_wider(names_from = word, values_from = n) %>%
+    mutate_all(~replace(., is.na(.), 0), vars = -group_cols())
   
   return (token_text)
   
