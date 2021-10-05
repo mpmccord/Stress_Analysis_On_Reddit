cf53df61e954c4e190d02e1c2c93703cfecc58d8
Melanie McCord <melanie.mccord19@ncf.edu>
2021-10-05 12:43:51 -0400

    Stop words now removed

--- create_bag_of_words_model.R
+++ create_bag_of_words_model.R
@@ -16,8 +16,8 @@ CleanText <- function(df) {
     unnest_tokens(word, text) %>%
     mutate(word = gsub('[[:punct:]]+','', word)) %>%
     mutate(word = gsub('\\<[[:digit:]]+\\>', '%d%', word)) %>%
-    mutate(word = paste("text", word, sep = '_')) %>%
-    anti_join(stop_words, by = c("word" = "word"))
+    anti_join(stop_words, by = c("word" = "word")) %>%
+    mutate(word = paste("text", word, sep = '_'))
   return(temp)
 }
 # CreateBagOfWords
